00e1e9e041023326bfd8acf2b125c55ae12745cc
Milan <mgcircle@hotmail.com>
2022-07-12 14:42:32 +0100

    test-edit

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+"test"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
