0a5f5de00f401c0d32142c1dd84c4fb9aef200bc
Milan <mgcircle@hotmail.com>
2022-07-19 13:41:14 +0100

    edit3

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-testing 123
+
 from pathlib import Path
 import os
 import environ
