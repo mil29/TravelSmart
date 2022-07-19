b1c2ea2c73b2a5519a824b3a01ecf97fae6bc2a9
Ubuntu <ubuntu@ip-172-31-41-202.eu-west-2.compute.internal>
2022-07-19 15:17:39 +0000

    edit

--- core/settings.py
+++ core/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
+#DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
