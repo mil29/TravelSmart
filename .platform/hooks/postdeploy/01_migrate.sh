7df22dd8708eaa187bf541145d222d4b74bfbd60
Milan <mgcircle@hotmail.com>
2022-07-21 19:00:03 +0100

    edits

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -5,3 +5,8 @@ python manage.py showmigrations;
 # migrate
 python manage.py migrate --noinput;
 }
+if [[ $EB_IS_COMMAND_LEADER == "true" ]];
+then   python manage.py migrate --noinput;  
+python manage.py collectstatic --noinput;
+else   echo "this instance is NOT the leader";
+fi...
