063d209ce7fbbf589e2b0ebf77bb67a999859d02
Milan <mgcircle@hotmail.com>
2022-07-21 18:38:59 +0100

    edits

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# source /var/app/venv/*/bin/activate
-# python /var/app/current/manage.py migrate --noinput
+source /var/app/venv/*/bin/activate
+cd /var/app/current
+python manage.py migrate --noinput
