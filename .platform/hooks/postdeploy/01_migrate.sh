a8961dfa57ed0bcf779a1e20957c84e393cbd82c
Milan <mgcircle@hotmail.com>
2022-07-22 11:33:36 +0100

    edits

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+source /var/app/venv/*/bin/activate 
+python /var/app/current/manage.py migrate --noinput
+
+
 # source "$PYTHONPATH/activate" && {
 # # log which migrations have already been applied
 # python manage.py showmigrations;
