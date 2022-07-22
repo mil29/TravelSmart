5c71a4e6b9f450c48576844422a15990bc2208ac
Milan <mgcircle@hotmail.com>
2022-07-22 15:50:56 +0100

    edit

--- core/settings.py
+++ core/settings.py
@@ -93,23 +93,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'travelsmart',
-#         'USER': env('POSTGRESQL_USER'),
-#         'PASSWORD': env('POSTGRESQL_PASSWORD'),
-#         'HOST': '3.10.53.250', # set to localhost when testing on ec2 then public_ip
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'travelsmart',
+        'USER': env('POSTGRESQL_USER'),
+        'PASSWORD': env('POSTGRESQL_PASSWORD'),
+        'HOST': '3.10.53.250', # set to localhost when testing on ec2 then public_ip
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
