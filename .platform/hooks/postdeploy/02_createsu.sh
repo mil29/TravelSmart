83faf8d7f3e56af35099a20eb4d550116f5bae30
Milan <mgcircle@hotmail.com>
2022-07-22 17:50:39 +0100

    edits createsu added

---
+++ .platform/hooks/postdeploy/02_createsu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /var/app/venv/*/bin/activate
+cd /var/app/current
+python manage.py createsu 
---
+++ users/management/commands/createsu.py
@@ -0,0 +1,15 @@
+from users.models import CustomUser
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Creates a superuser.'
+
+    def handle(self, *args, **options):
+        if not CustomUser.objects.filter(username='admin').exists():
+            CustomUser.objects.create_superuser(
+                email="admin123@admin.com",
+                username='admin123',
+                password='password7791'
+            )
+        print('Superuser has been created.')
