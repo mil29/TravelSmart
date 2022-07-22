6c7c27fd8450b8e5c9a55f8c3af9135766b7c990
Milan <mgcircle@hotmail.com>
2022-07-22 11:49:12 +0100

    edits

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 source /var/app/venv/*/bin/activate 
-python /var/app/current/manage.py migrate --noinput
-
-
-# source "$PYTHONPATH/activate" && {
-# # log which migrations have already been applied
-# python manage.py showmigrations;
-# # migrate
-# python manage.py migrate --noinput;
-# }
-# if [[ $EB_IS_COMMAND_LEADER == "true" ]];
-# then   python manage.py migrate --noinput;  
-# python manage.py collectstatic --noinput;
-# else   echo "this instance is NOT the leader";
-# fi...
+# python /var/app/current/manage.py migrate --noinput
