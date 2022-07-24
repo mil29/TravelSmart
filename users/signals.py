d13632c43f471492d487cf879e2ff0894f6e435b
Milan <mgcircle@hotmail.com>
2022-07-24 17:15:20 +0100

    edit

--- users/signals.py
+++ users/signals.py
@@ -29,8 +29,8 @@ user_logged_in.connect(login_user)
 @receiver(post_delete, sender=Profile)
 def profile_image_delete(sender, instance, **kwargs):
     if instance.profie_pic:
-        if os.path.isfile(instance.profile_pic):
-            os.remove(instance.profile_pic)
+        if os.path.isfile(instance.profile_pic.path):
+            os.remove(instance.profile_pic.path)
 
 
 
@@ -45,8 +45,8 @@ def profile_image_update(sender, instance, **kwargs):
         new_image = instance.profile_pic
         # check old image is not new image then overwrites if old iamge is default image then ignores
         if not old_image == new_image and 'default_profile_pic/default.jpeg' not in old_image:
-            if os.isfile(old_image):
-                os.remove(old_image)
+            if os.name.isfile(old_image.name):
+                os.remove(old_image.name)
     else:
         return False
 
