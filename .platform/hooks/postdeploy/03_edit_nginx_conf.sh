9eba65a0fe2cb487e5eed1a2bb3ea1cae6586804
Milan <mgcircle@hotmail.com>
2022-07-25 18:11:13 +0100

    edit

--- .platform/hooks/postdeploy/03_edit_nginx_conf.sh
+++ .platform/hooks/postdeploy/03_edit_nginx_conf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo sed -i '14 i \   client_max_body_size 100M;' /etc/nginx/nginx.conf
+sudo sed -i '14 i \    client_max_body_size 100M;' /etc/nginx/nginx.conf
 sudo service nginx restart
