e21d4e1c1efb8fc85919a5c38409921d5ddb1573
Milan <mgcircle@hotmail.com>
2022-07-28 16:34:16 +0100

    edit

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /var/app/venv/*/bin/activate
 cd /var/app/current
-python manage.py migrate
-python manage.py collectstatic --noinput
+# python manage.py migrate
+# python manage.py collectstatic --noinput
