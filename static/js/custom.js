cffb7e1208590ee78d00daf5ee784b1de1b7aaa6
Milan <mgcircle@hotmail.com>
2022-07-29 15:41:16 +0100

    Edit

--- static/js/custom.js
+++ static/js/custom.js
@@ -52,7 +52,7 @@ $(document).ready(function() {
         }
         var baseUrl = "/img/flags";
         var $state = $(
-            `<span><img src="https://travel-smart.s3.eu-west-2.amazonaws.com/img/flags/${state.element.value.slice(0,3).toLowerCase()}.png" id="img-flag" /> ${state.text}</span>`
+            `<span><img src="{% static '${baseUrl}/${state.element.value.slice(0,3).toLowerCase()}.png' %}" id="img-flag" /> ${state.text}</span>`
         );
         return $state;
     };
