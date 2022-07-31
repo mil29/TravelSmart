636367225ad73ca9a803c25b56db1d7d09a41882
Milan <mgcircle@hotmail.com>
2022-07-31 17:02:04 +0100

    edit

--- core/settings.py
+++ core/settings.py
@@ -108,8 +108,8 @@ if DEBUG == False:
         'NAME': os.environ['POSTGRES_DB_NAME'],
         'USER': os.environ['POSTGRES_USERNAME'],
         'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-        'HOST': os.environ['POSTGRES_HOSTNAME'],
-        'PORT': os.environ['POSTGRES_PORT'],
+        'HOST': '18.132.119.18',
+        'PORT': '5432',
         }
     }
 else:
